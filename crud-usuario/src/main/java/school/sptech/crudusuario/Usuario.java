d5ddb8801141f188c4e0e4f658c4d1b92f3f7f2d
fernandofs21 <fernando.silva@sptech.school>
2025-03-02 13:15:52 -0300

    feat: adicionar crud de usuário

---
+++ crud-usuario/src/main/java/school/sptech/crudusuario/Usuario.java
@@ -0,0 +1,57 @@
+package school.sptech.crudusuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String cargo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
---
+++ crud-usuario/src/main/java/school/sptech/crudusuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package school.sptech.crudusuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
