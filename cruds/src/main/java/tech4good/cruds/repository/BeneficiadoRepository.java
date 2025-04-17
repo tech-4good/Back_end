da9c044c4da055723b0fb1a3f66a8d4c647ab995
fernandofs21 <fernando.silva@sptech.school>
2025-04-17 11:36:02 -0300

    fix(BeneficiadoRepository.java): arrumar pesquisas de cpf

--- cruds/src/main/java/tech4good/cruds/repository/BeneficiadoRepository.java
+++ cruds/src/main/java/tech4good/cruds/repository/BeneficiadoRepository.java
@@ -1,11 +1,12 @@
 package tech4good.cruds.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tech4good.cruds.entity.Beneficiado;
 
 public interface BeneficiadoRepository extends JpaRepository<Beneficiado, Integer> {
 
-    public Beneficiado findByCpf(String cpf);
+    public Beneficiado findByIdCpf(String cpf);
 
-    public Boolean existsByCpf(String cpf);
+    public Boolean existsByIdCpf(String cpf);
 }
