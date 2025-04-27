93598abbc16c4ea6b29853196f137afbdd7354ed
ViniciusMiralha <vinicius.gomes@sptech.school>
2025-04-27 19:34:44 -0300

    feat: criar CestaController

--- cruds/src/main/java/tech4good/cruds/controller/CestaController.java
+++ cruds/src/main/java/tech4good/cruds/controller/CestaController.java
@@ -2,7 +2,6 @@ package tech4good.cruds.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tech4good.cruds.repository.CestaRepository;
 import tech4good.cruds.entity.Cesta;
 import tech4good.cruds.service.CestaService;
 
@@ -18,53 +17,42 @@ public class CestaController {
         this.cestaService = cestaService;
     }
 
-    /*
     @PostMapping
     public ResponseEntity<Cesta> cadastrar(
             @RequestBody Cesta cestaParaCadastrar
     ) {
-        Cesta cestaCadastrada = repository.save(cestaParaCadastrar);
-
-        return ResponseEntity.status(201).body(cestaCadastrada);
+        Cesta cestaRegistrada = cestaService.cadastrarCesta(cestaParaCadastrar);
+        return ResponseEntity.status(201).body(cestaRegistrada);
     }
 
     @GetMapping
     public ResponseEntity<List<Cesta>> listar() {
-        List<Cesta> cestasEncontradas = repository.findAll();
-
-        if (cestasEncontradas.isEmpty()) {
+        List<Cesta> cestas = cestaService.listarCestas();
+        if (cestas.isEmpty()) {
             return ResponseEntity.status(204).build();
         }
+        return ResponseEntity.status(200).body(cestas);
+    }
 
-        return ResponseEntity.status(200).body(cestasEncontradas);
+    @GetMapping("/{id}")
+    public ResponseEntity<Cesta> buscarPorId(@PathVariable Integer id) {
+        Cesta cestaEncontrada = cestaService.buscarCestaPorId(id);
+        return ResponseEntity.status(200).body(cestaEncontrada);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<Cesta> atualizar(
-            @RequestBody Cesta cestaNova, @PathVariable Integer id
+            @RequestBody Cesta cestaNova,
+            @PathVariable Integer id
     ) {
-        if (repository.existsById(id)) {
-            cestaNova.setIdCesta(id);
-            Cesta cestaAlterada = repository.save(cestaNova);
-
-            return ResponseEntity.status(200).body(cestaAlterada);
-        }
-
-        return ResponseEntity.status(404).build();
+        cestaNova.setIdCesta(id);
+        Cesta cestaAlterada = cestaService.atualizarCestaPorId(cestaNova);
+        return ResponseEntity.status(200).body(cestaAlterada);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletar(
-            @PathVariable Integer id
-    ) {
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
-
-            return ResponseEntity.status(200).build();
-        }
-
-        return ResponseEntity.status(404).build();
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        cestaService.removerCesta(id);
+        return ResponseEntity.status(204).build();
     }
-    */
 }
-
