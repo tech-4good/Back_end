068a091eac6ccb3501dcd1fb7eecf4feac576cfc
ViniciusMiralha <vinicius.gomes@sptech.school>
2025-04-27 21:29:12 -0300

    feat: adicionar EntregaController

--- cruds/src/main/java/tech4good/cruds/controller/EntregaController.java
+++ cruds/src/main/java/tech4good/cruds/controller/EntregaController.java
@@ -1,8 +1,11 @@
 package tech4good.cruds.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech4good.cruds.entity.Entrega;
 import tech4good.cruds.service.EntregaService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/entregas")
@@ -12,5 +15,40 @@ public class EntregaController {
 
     public EntregaController(EntregaService entregaService) {
         this.entregaService = entregaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Entrega> cadastrar(@RequestBody Entrega entrega) {
+        Entrega novaEntrega = entregaService.cadastrarEntrega(entrega);
+        return ResponseEntity.status(201).body(novaEntrega);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Entrega> buscarPorId(@PathVariable Integer id) {
+        Entrega entrega = entregaService.buscarEntregaPorId(id);
+        return ResponseEntity.ok(entrega);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Entrega>> listar() {
+        List<Entrega> entregas = entregaService.listarEntregas();
+        return entregas.isEmpty() ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.ok(entregas);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Entrega> atualizar(
+            @PathVariable Integer id,
+            @RequestBody Entrega entrega) {
+        entrega.setIdEntrega(id);
+        Entrega entregaAtualizada = entregaService.atualizarEntrega(entrega);
+        return ResponseEntity.ok(entregaAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Integer id) {
+        entregaService.removerEntregaPorId(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- cruds/src/main/java/tech4good/cruds/service/EntregaService.java
+++ cruds/src/main/java/tech4good/cruds/service/EntregaService.java
@@ -39,8 +39,8 @@ public class EntregaService {
                     formatted(entrega.getIdEntrega()));
         }
     }
-
-    public void removerBeneficiadoPorId(Integer id){
+//Alterei o nome de removerBeneficiadoPorId para removerEntregaPorId
+    public void removerEntregaPorId(Integer id){
         if(entregaRepository.existsById(id)){
             entregaRepository.deleteById(id);
         } else {
