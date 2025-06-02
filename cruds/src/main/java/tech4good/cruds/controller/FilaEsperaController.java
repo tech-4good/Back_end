bcb66f141860c26dacfd00b0a332a6eafad87ab6
ViniciusMiralha <vinicius.gomes@sptech.school>
2025-06-02 01:12:57 -0300

    feat: listar fila de espera

--- cruds/src/main/java/tech4good/cruds/controller/FilaEsperaController.java
+++ cruds/src/main/java/tech4good/cruds/controller/FilaEsperaController.java
@@ -4,6 +4,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech4good.cruds.service.FilaEsperaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import tech4good.cruds.entity.FilaEspera;
+import java.util.List;
 
 @Tag(name = "Controller - Fila de Espera", description = "Operações relacionadas a fila de espera das cestas básicas.")
 @RestController
@@ -14,5 +18,14 @@ public class FilaEsperaController {
 
     public FilaEsperaController(FilaEsperaService filaEsperaService) {
         this.filaEsperaService = filaEsperaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FilaEspera>> listar() {
+        List<FilaEspera> fila = filaEsperaService.listarFilaEspera();
+        if (fila.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(fila);
     }
 }
--- cruds/src/main/java/tech4good/cruds/service/FilaEsperaService.java
+++ cruds/src/main/java/tech4good/cruds/service/FilaEsperaService.java
@@ -2,6 +2,8 @@ package tech4good.cruds.service;
 
 import org.springframework.stereotype.Service;
 import tech4good.cruds.repository.FilaEsperaRepository;
+import tech4good.cruds.entity.FilaEspera;
+import java.util.List;
 
 @Service
 public class FilaEsperaService {
@@ -10,6 +12,10 @@ public class FilaEsperaService {
 
     public FilaEsperaService(FilaEsperaRepository filaEsperaRepository) {
         this.filaEsperaRepository = filaEsperaRepository;
+    }
+
+    public List<FilaEspera> listarFilaEspera() {
+        return filaEsperaRepository.findAll();
     }
 
 }
