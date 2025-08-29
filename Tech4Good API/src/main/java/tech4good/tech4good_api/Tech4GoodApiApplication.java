e9597e716a05de6793de518de14300a38f40f141
fernandofs21 <fernando.silva@sptech.school>
2025-08-29 17:22:38 -0300

    feat: configurar swagger no main

--- Tech4Good API/src/main/java/tech4good/tech4good_api/Tech4GoodApiApplication.java
+++ Tech4Good API/src/main/java/tech4good/tech4good_api/Tech4GoodApiApplication.java
@@ -1,13 +1,43 @@
 package tech4good.tech4good_api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API de Doações - Projeto ASA",
+                version = "1.0.0",
+                description = "Sistema de gerenciamento de arrecadação e distribuição de cestas básicas para famílias em vulnerabilidade social  \n" +
+                        "\nDesign Pattern escolhido: Adapter.\n" +
+                        "\nIntegrantes:\n- Bruna Santana Reginato\n- Fernando Felix da Silva" +
+                        "\n- Ilys Gomes Maroto\n- Lucas Alves Matos\n- Matheus Yukio Makiyama\n- Vinicius Miralha Augusto Gomes\n\n" +
+                        "![Logo do Projeto](https://raw.githubusercontent.com/tech-4good/Documentation/main/logo-tech4good-2.png)" +
+                        "![Logo da ASA](https://raw.githubusercontent.com/tech-4good/Documentation/main/logo-asa.png)\n\n",
+                contact = @Contact(
+                        name = "Equipe do Projeto",
+                        email = "PesquisaeInovao-ProjetodeExtenso-Grupo5@sptech.school"
+                )
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "Servidor Local"
+                )
+        }
+)
+
+@EnableFeignClients
 @SpringBootApplication
 public class Tech4GoodApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tech4GoodApiApplication.class, args);
+        System.out.println("http://localhost:8080/swagger-ui/index.html");
 	}
 
 }
