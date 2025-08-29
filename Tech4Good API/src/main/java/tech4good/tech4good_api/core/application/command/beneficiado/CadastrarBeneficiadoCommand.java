1926a4ded1cf0189df85382b9732e5569c441747
fernandofs21 <fernando.silva@sptech.school>
2025-08-29 14:17:33 -0300

    feat: criar command de CadastrarBeneficiadoCommand

---
+++ Tech4Good API/src/main/java/tech4good/tech4good_api/core/application/command/beneficiado/CadastrarBeneficiadoCommand.java
@@ -0,0 +1,30 @@
+package tech4good.tech4good_api.core.application.command.beneficiado;
+
+import tech4good.tech4good_api.core.domain.beneficiado.valueobject.EstadoCivil;
+import tech4good.tech4good_api.core.domain.beneficiado.valueobject.Religiao;
+import tech4good.tech4good_api.core.domain.beneficiado.valueobject.Renda;
+import tech4good.tech4good_api.core.domain.beneficiado.valueobject.Rg;
+import tech4good.tech4good_api.core.domain.endereco.Endereco;
+import tech4good.tech4good_api.core.domain.shared.valueobject.Cpf;
+import tech4good.tech4good_api.core.domain.shared.valueobject.Telefone;
+
+import java.time.LocalDate;
+
+public record CadastrarBeneficiadoCommand(
+    Cpf cpf,
+    String nome,
+    Rg rg,
+    LocalDate dataNascimento,
+    String naturalidade,
+    Telefone telefone,
+    EstadoCivil estadoCivil,
+    String escolaridade,
+    String profissao,
+    Renda rendaMensal,
+    String empresa,
+    String cargo,
+    Religiao religiao,
+    Endereco endereco,
+    Integer quantidadeDependentes
+) {
+}
