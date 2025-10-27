44308bad4477c3dc87470ee9897735c01ce44762
mathyukio <matheus.makiyama@sptech.school>
2025-10-27 18:09:22 -0300

    refactor: remocao da fluxo da FilaEspera

--- Tech4Good_API/src/main/java/tech4good/tech4good_api/core/application/command/filaespera/BuscarFilaEsperaPorIdCommand.java
+++
@@ -1,3 +0,0 @@
-package tech4good.tech4good_api.core.application.command.filaespera;
-
-public record BuscarFilaEsperaPorIdCommand(Integer id) {}
--- Tech4Good_API/src/main/java/tech4good/tech4good_api/core/application/command/filaespera/CadastrarFilaEsperaCommand.java
+++
@@ -1,6 +0,0 @@
-package tech4good.tech4good_api.core.application.command.filaespera;
-
-import java.time.LocalDate;
-import tech4good.tech4good_api.core.domain.beneficiado.Beneficiado;
-
-public record CadastrarFilaEsperaCommand(LocalDate dataEntradaFila, Beneficiado beneficiado) {}
