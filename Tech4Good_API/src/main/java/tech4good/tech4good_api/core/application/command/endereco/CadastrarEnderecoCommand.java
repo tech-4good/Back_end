e0de30b3cd76a78f61c9aa305d4c378d01e34e0e
mathyukio <matheus.makiyama@sptech.school>
2025-12-01 21:20:51 -0300

    fix: debugando branch

--- Tech4Good_API/src/main/java/tech4good/tech4good_api/core/application/command/endereco/CadastrarEnderecoCommand.java
+++ Tech4Good_API/src/main/java/tech4good/tech4good_api/core/application/command/endereco/CadastrarEnderecoCommand.java
@@ -3,11 +3,8 @@ package tech4good.tech4good_api.core.application.command.endereco;
 import tech4good.tech4good_api.core.domain.endereco.valueobjects.*;
 import tech4good.tech4good_api.core.domain.shared.valueobject.TipoCesta;
 
-<<<<<<< HEAD
-=======
  import java.time.LocalDate;
 
->>>>>>> main
 public record CadastrarEnderecoCommand(
  String logradouro,
  String numero,
